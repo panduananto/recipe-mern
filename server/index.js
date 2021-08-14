4e49982dc81d6bcd0233e46b56e8ea1ca13ea5cb
Pandu Ananto <59494215+panduananto@users.noreply.github.com>
2021-08-14 14:06:42 +0700

    cra, tailwindcss, express, connection to mongoose

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const CONNECTION_URL = 'mongodb+srv://pandu:pandu123@cluster0.itnql.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3001;
+const MONGOOSE_OPTION = { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+mongoose
+  .connect(CONNECTION_URL, MONGOOSE_OPTION)
+  .then(() => app.listen(PORT, () => console.log(`Running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
