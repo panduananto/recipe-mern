508e7f0926967fecd782326b68be45ca1f0613ba
Pandu Ananto <59494215+panduananto@users.noreply.github.com>
2021-09-14 21:00:39 +0700

    user model

---
+++ server/src/models/user.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide a username'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email address'],
+    unique: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
